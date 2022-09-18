0e0853f1305ecfad59a38e94af07c01d02730c02
NezertorcheaT <62296095+Grisha06@users.noreply.github.com>
2022-09-18 19:10:50 +0300

    Add files via upload

---
+++ Scripts/Player.py
@@ -0,0 +1,29 @@
+import keyboard
+
+from NTEngineClasses import *
+
+
+class Player(Behavior):
+    speed = 0.5
+
+    def start(self):
+        ui.add("", True)
+        ui.add("", True)
+        ui.add("", True)
+
+    def update(self, a):
+        if keyboard.is_pressed("w"):
+            self.gm.tr.moveDir(Vec3(0, -self.speed))
+        if keyboard.is_pressed("a"):
+            self.gm.tr.moveDir(Vec3(-self.speed, 0))
+        if keyboard.is_pressed("s"):
+            self.gm.tr.moveDir(Vec3(0, self.speed))
+        if keyboard.is_pressed("d"):
+            self.gm.tr.moveDir(Vec3(self.speed, 0))
+        if keyboard.is_pressed("e"):
+            Behavior.instantiate("r", Vec3.sum(self.gm.tr.getPosition(), Vec3(1)))
+
+    def onDraw(self, a):
+        ui.changeSpace(0, str(self.gm.tr.getPosition().x), True)
+        ui.changeSpace(1, str(self.gm.tr.getPosition().y), True)
+        ui.changeSpace(2, str(self.gm.GetComponent(BoxCollider).collide), True)
