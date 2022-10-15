1e53346e25f2893fb4a8f60d81e26400c83a50e2
NezertorcheaT <62296095+Grisha06@users.noreply.github.com>
2022-10-16 00:12:15 +0300

    Add files via upload

---
+++ Scripts/Enemy.py
@@ -0,0 +1,56 @@
+import Scripts.Player
+from NTEngineClasses import *
+
+
+class Enemy(Behavior):
+    speed = 0.5
+    coll = None
+    anim = None
+    f: Vector3
+    fr = False
+    fram = 0
+
+    def start(self):
+        ui.add("", True)
+        ui.add("", True)
+        ui.add("", True)
+        self.coll = self.gameobject.GetComponent(Collider)
+        self.f = Vector3()
+        self.anim = TypedList(type_of=list, data=[])
+        self.fram = 0
+        self.anim.append(images["enemw1"])
+        self.anim.append(images["enemw1"])
+        self.anim.append(images["enemw1"])
+        self.anim.append(images["enemw1"])
+        self.anim.append(images["enemw1"])
+        self.anim.append(images["enemw1"])
+        self.anim.append(images["enemw2"])
+        self.anim.append(images["enemw2"])
+        self.anim.append(images["enemw2"])
+        self.anim.append(images["enemw2"])
+        self.anim.append(images["enemw2"])
+        self.anim.append(images["enemw2"])
+        # self.gameobject.GetComponent(Drawer).color = "Blue"
+
+    def update(self, a):
+        self.transform.moveDir(self.f % self.speed)
+        self.f = Vector3()
+
+        for i in findAllObjsAtRad(self.transform.position, 1000):
+            if i.GetComponent(Scripts.Player.Player) is not None:
+                self.f = (i.transform.position-self.gameobject.transform.position).norm()
+
+        self.fr = not self.fr
+        self.fram += 1
+        if self.fram == len(self.anim):
+            self.fram = 0
+
+    def onDraw(self, a):
+        if not self.coll.collide:
+            self.gameobject.GetComponent(Drawer).drawSymbImage(a, self.anim[self.fram],
+                                                               self.gameobject.transform.position)
+        else:
+            self.gameobject.GetComponent(Drawer).drawSymbImage(a, images["enem"], self.gameobject.transform.position)
+        ui.printStrAtPos(str(self.f), 0, 25)
+
+    # UI.printImageAtPos("table", self.transform.position.x, self.transform.position.y)
--- Scripts/Player.py
+++ Scripts/Player.py
@@ -6,6 +6,9 @@ from NTEngineClasses import *
 class Player(Behavior):
     speed = 0.5
     coll = None
+    fr = False
+    wa = False
+
     def __init__(self, gameobject):
         super().__init__(gameobject)
         self.speed = 0.5
@@ -16,25 +19,42 @@ class Player(Behavior):
         ui.add("", True)
         self.coll = self.gameobject.GetComponent(Collider)
         self.f = Vector3()
+        self.anim = TypedList(type_of=list, data=[])
+        self.fram = 0
+        self.anim.append(images["plw1"])
+        self.anim.append(images["plw1"])
+        self.anim.append(images["plw1"])
+        self.anim.append(images["plw2"])
+        self.anim.append(images["plw2"])
+        self.anim.append(images["plw2"])
         # self.gameobject.GetComponent(Drawer).color = "Blue"
 
     def update(self, a):
         self.transform.moveDir(self.f)
+
         # self.transform.local_position = Vec3.int(self.transform.local_position)
         self.f = Vector3()
-
+        self.wa = False
         if keyboard.is_pressed("w"):
             self.f = self.f + Vector3(0, -self.speed)
+            self.wa = True
+            self.fr = not self.fr
         if keyboard.is_pressed("a"):
             self.f = self.f + Vector3(-self.speed, 0)
+            self.wa = True
+            self.fr = not self.fr
         if keyboard.is_pressed("s"):
             self.f = self.f + Vector3(0, self.speed)
+            self.wa = True
+            self.fr = not self.fr
         if keyboard.is_pressed("d"):
             self.f = self.f + Vector3(self.speed, 0)
-        # if self.coll.collide:
-        #    f = f % -1
+            self.wa = True
+            self.fr = not self.fr
+        self.fram += 1
+        if self.fram == len(self.anim):
+            self.fram = 0
 
-        # self.f = self.f + (Vec3.D2V(self.coll.angl) % self.speed)
         if keyboard.is_pressed("r"):
             Behavior.instantiate("r", self.transform.position + Vector3(0, 1), [BoxCollider])
 
@@ -42,6 +62,10 @@ class Player(Behavior):
         ui.changeSpace(0, str(self.transform.position), True)
         ui.changeSpace(1, str(self.coll), True)
         ui.changeSpace(2, str(Vector3.D2V(self.coll.angle)), True)
-        self.gameobject.GetComponent(Drawer).drawSymbImage(a, images["pl"], self.gameobject.transform.position)
+        if self.wa:
+            self.gameobject.GetComponent(Drawer).drawSymbImage(a, self.anim[self.fram],
+                                                               self.gameobject.transform.position)
+        else:
+            self.gameobject.GetComponent(Drawer).drawSymbImage(a, images["pl"], self.gameobject.transform.position)
 
     # UI.printImageAtPos("table", self.transform.position.x, self.transform.position.y)
